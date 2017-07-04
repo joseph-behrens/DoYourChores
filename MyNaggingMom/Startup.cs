e2e93096f21c1258d3459c5bca513a5a7b873bd9
Joe Behrens <Joe.Behrens@rbaconsulting.com>
2017-07-04 13:05:01 -0500

    Add project files.

---
+++ MyNaggingMom/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyNaggingMom.Startup))]
+namespace MyNaggingMom
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
