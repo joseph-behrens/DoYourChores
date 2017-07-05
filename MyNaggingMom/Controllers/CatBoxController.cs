6e43bc9fa67f09baf2d409198829045582eb9358
Joe Behrens <Joe.Behrens@rbaconsulting.com>
2017-07-04 20:43:45 -0500

    Setup CatBox page and Controller

---
+++ MyNaggingMom/Controllers/CatBoxController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyNaggingMom.Models;
+
+namespace MyNaggingMom.Controllers
+{
+    public class CatBoxController : Controller
+    {
+        private CatBox catBox = new CatBox();
+        
+        public ActionResult Clean(CatBox catBox)
+        {
+            return View(catBox);
+        }
+    }
+}
---
+++ MyNaggingMom/Models/CatBox.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyNaggingMom.Models
+{
+    public class CatBox
+    {
+        public DateTime LastTimeCleaned { get; set; }
+        public DateTime TimeToCleanAgain { get; set; }
+
+
+        // Constructor
+        public CatBox()
+        {
+            LastTimeCleaned = DateTime.Now;
+            TimeToCleanAgain = LastTimeCleaned.AddDays(1);
+        }
+    }
+}
